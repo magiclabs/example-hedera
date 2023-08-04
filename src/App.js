af7c3c0918c248c9b3fa539b945017643d307853
Hunter Cote <huntercote2@gmail.com>
2023-08-04 13:01:26 -0400

    use OTP

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export default function App() {
   }, [isLoggedIn]);
 
   const login = async () => {
-    await magic.auth.loginWithMagicLink({ email });
+    await magic.auth.loginWithEmailOTP({ email });
     setIsLoggedIn(true);
   };
 
